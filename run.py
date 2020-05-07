07a0cf6f1e22bebe0f784e6951b86ccb96507ef4
Adrian <acidade@users.noreply.github.com>
2020-05-07 23:33:02 +0200

    logging into similarbands.log in main folder

--- run.py
+++ run.py
@@ -1,20 +1,19 @@
 import get_videos
 import get_bands
-import os
 import logging
 
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logging.basicConfig(filename='similarbands.log', level=logging.INFO, format='%(levelname)s:%(name)s:%(asctime)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 log = logging.getLogger(__name__)
 
 message = f'---- START: {get_bands.timestamp} ----'
 print(message)
-log.debug(message)
+log.info(message)
 
 # get videos from YouTube playlists
 message = f'Getting videos from YouTube...'
 print(message)
-log.debug(message)
+log.info(message)
 
 df = get_videos.get_yt_likes()
 videos = get_videos.write_videosheet(df)
@@ -22,29 +21,29 @@ videos = get_videos.write_videosheet(df)
 if videos[0] == True:
 	message = f'Step 1 SUCCESS: {videos[1]}'
 	print(message)
-	log.debug(message)
+	log.info(message)
 else:
 	message = f'Step 1 FAIL: {videos[1]}'
 	print(message)
-	log.debug(message)
+	log.info(message)
 
 
 # get similar bands (execute script)
 message = 'Getting similar bands from last.fm...'
 print(message)
-log.debug(message)
+log.info(message)
 
 bands = get_bands.get_bands()
 
 if bands[0]==True:
 	message = f'Step 2 SUCCESS: {bands[1]}'
 	print(message)
-	log.debug(message)
+	log.info(message)
 else:
 	message = f'Step 2 FAIL: {bands[1]}'
 	print(message)
-	log.debug(message)
+	log.info(message)
 
 message = '---- DONE ----'
 print(message)
-log.debug(message)
+log.info(message)
